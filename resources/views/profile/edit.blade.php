26228d89550be1c066ed63123a7efda429beb657
wigscs <174610664+wigscs@users.noreply.github.com>
2024-10-02 10:44:00 +0100

    remove registration and user deletion

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -19,11 +19,16 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+
+            {{-- Only show delete section if destroy route exists --}}
+            @if (Route::has('profile.destroy'))
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.delete-user-form')
+                    </div>
                 </div>
-            </div>
+            @endif
+
         </div>
     </div>
 </x-app-layout>
